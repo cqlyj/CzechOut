c0d0fab9350486f6c19a4e4e46a1b3dfe5bbf046
Fu <frederic010992@gmail.com>
2025-05-31 15:46:16 +0200

    human login -2

--- vlayer/src/shared/layout/Layout.tsx
+++ vlayer/src/shared/layout/Layout.tsx
@@ -5,13 +5,8 @@ import { ConnectWallet } from "../../pages/connectWallet";
 
 export const Layout = () => {
   return (
-    <div className="relative">
-      <div className="fixed top-4 right-4 z-[9999] pointer-events-auto">
-        <ConnectWallet />
-      </div>
       <Modal>
         <Outlet />
       </Modal>
-    </div>
   );
 };
