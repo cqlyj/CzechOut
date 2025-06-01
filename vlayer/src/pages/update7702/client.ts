caffc20b80e9116bd08d86d4ac15134d39da2d68
cqlyj <luoyingjie0721@gmail.com>
2025-06-01 06:55:39 +0200

    Update client.ts to change relay private key and format code for improved readability.

--- vlayer/src/pages/update7702/client.ts
+++ vlayer/src/pages/update7702/client.ts
@@ -1,11 +1,15 @@
-import { createWalletClient, http } from 'viem'
-import { privateKeyToAccount } from 'viem/accounts'
-import { mainnet, sepolia } from 'viem/chains'
- 
-const relay = privateKeyToAccount('0x21f6ad4a9bcab0cf664e19f0cf0682aad455f43de3721710a1ea50519017b218')
- 
+import { createWalletClient, http } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { mainnet, sepolia } from "viem/chains";
+
+const relay = privateKeyToAccount(
+  "0x344f59f3270a57ac589cce3a23b9f75bb665e0be31f4ed4cb238ed5e03d9318b"
+);
+
 export const walletClient = createWalletClient({
   account: relay,
   chain: sepolia,
-  transport: http("https://eth-sepolia.g.alchemy.com/v2/Wo0LTBce7DBLE8KZKQC6UsN4cyeI-WKm"),
-})
+  transport: http(
+    "https://eth-sepolia.g.alchemy.com/v2/Wo0LTBce7DBLE8KZKQC6UsN4cyeI-WKm"
+  ),
+});
